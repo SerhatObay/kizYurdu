3901731a0686ebbf7684e3d9038c2264edb77991
Serhat Obay <serhat.obay2121@gmail.com>
2022-06-22 11:36:13 +0300

    bug fixed

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about',function (){
+    return view('about');
+});
+Route::get('/contact',function (){
+    return view('contact');
+});
